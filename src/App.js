c5f49c315142dd638c61895f5c76f9cbc80617e7
scm-minaungpaing <min.aungpaing@seattleconsultingmyanmar.com>
2022-01-05 00:34:37 +0630

    react todo list tutorial

--- src/App.js
+++ src/App.js
@@ -10,41 +10,37 @@ function App() {
   const [status, setStatus] = useState("all")
   const [filterTodo, setFilterTodo] = useState([])
 
-  
-
-  const filterHandler = () => {
-    switch (status) {
-      case "completed":
-        setFilterTodo(todos.filter((todo) => todo.complete))
-        break;
-      case "uncompleted":
-        setFilterTodo(todos.filter((todo) => !todo.complete))
-        break;
-      default:
-        setFilterTodo(todos)
-        break;
-    }
-  }
-
-  const saveLocalTodos = () => {
-    localStorage.setItem("todos", JSON.stringify(todos))
-  }
-
-  const getLocalTodos = () => {
-    // if (localStorage.getItem("todos") === null) {
-    //   localStorage.setItem("todos", JSON.stringify([]));
-    // } else {
-      let localTodo = JSON.parse(localStorage.getItem("todos"));
-      setTodos(localTodo)
-    // }
-  }
-
   useEffect(() => {
+    const getLocalTodos = () => {
+      // if (localStorage.getItem("todos") === null) {
+      //   localStorage.setItem("todos", JSON.stringify([]));
+      // } else {
+        let localTodo = JSON.parse(localStorage.getItem("todos"));
+        setTodos(localTodo)
+      // }
+    }
     getLocalTodos();
   }, [])
 
   useEffect(() => {
+    const filterHandler = () => {
+      switch (status) {
+        case "completed":
+          setFilterTodo(todos.filter((todo) => todo.complete))
+          break;
+        case "uncompleted":
+          setFilterTodo(todos.filter((todo) => !todo.complete))
+          break;
+        default:
+          setFilterTodo(todos)
+          break;
+      }
+    }
     filterHandler();
+
+    const saveLocalTodos = () => {
+      localStorage.setItem("todos", JSON.stringify(todos))
+    }
     saveLocalTodos();
   }, [ todos, status])
 
